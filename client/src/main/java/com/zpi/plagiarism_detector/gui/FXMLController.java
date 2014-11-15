989ca0c9e54aa4cf16ecb4cdcb3f4049323c4c39
agat48 <agat.paciorek@gmail.com>
2014-11-15 09:26:44 +0100

    GUI code review window

--- client/src/main/java/com/zpi/plagiarism_detector/gui/FXMLController.java
+++ client/src/main/java/com/zpi/plagiarism_detector/gui/FXMLController.java
@@ -1,14 +1,19 @@
 package com.zpi.plagiarism_detector.gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
 
 public class FXMLController implements Initializable {
 
@@ -19,7 +24,7 @@ public class FXMLController implements Initializable {
     private TextArea inputData, outputData;
 
     @FXML
-    Button checkButton;
+    Button checkButton, codeReviewButton;
 
     @FXML
     private void handleButtonAction(ActionEvent event) {
@@ -29,6 +34,26 @@ public class FXMLController implements Initializable {
         outputData.selectRange(11, outputData.getLength());
 
         label.setText("Hello Guys! ;)");
+    }
+
+    @FXML
+    private void handleCodeReviewButtonAction(ActionEvent event) {
+        Parent root;
+        System.out.println("You clicked me, bastard!");
+        URL url = getClass().getResource("/fxml/CodeScene.fxml");
+        try {
+            root = FXMLLoader.load(url);
+            Stage stage = new Stage();
+            stage.setTitle("Code source review");
+            Scene scene = new Scene(root, 600, 500);
+            scene.getStylesheets().add("/styles/Styles.css");
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
