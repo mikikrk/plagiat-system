ee9277d6c59ffecde1ee0e243970b7c668af96a6
mikikrk <miki.krk@gmail.com>
2014-11-16 09:22:20 +0100

    Klasy do przesyłania wiadomości między serwerem a klientem

--- commons/src/main/java/com/zpi/plagiarism_detector/commons/protocol/DocumentData.java
+++ commons/src/main/java/com/zpi/plagiarism_detector/commons/protocol/DocumentData.java
@@ -8,4 +8,39 @@ public class DocumentData implements Serializable {
     private List<String> keywords;
     private String article;
     private List<String> codes;
+	public DocumentData(String title, List<String> keywords, String article,
+			List<String> codes) {
+		super();
+		this.title = title;
+		this.keywords = keywords;
+		this.article = article;
+		this.codes = codes;
+	}
+	public DocumentData(){
+		super();
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public List<String> getKeywords() {
+		return keywords;
+	}
+	public void setKeywords(List<String> keywords) {
+		this.keywords = keywords;
+	}
+	public String getArticle() {
+		return article;
+	}
+	public void setArticle(String article) {
+		this.article = article;
+	}
+	public List<String> getCodes() {
+		return codes;
+	}
+	public void setCodes(List<String> codes) {
+		this.codes = codes;
+	}
 }
