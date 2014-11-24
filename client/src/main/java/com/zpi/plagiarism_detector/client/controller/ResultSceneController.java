acb373282733f1942c333db7772fdbba012cc229
mikikrk <miki.krk@gmail.com>
2014-11-24 22:53:46 +0100

    Metody do liczenia statystyk, oraz rozbicie listy wyników na dodatkową listę dla każdego pliku

--- client/src/main/java/com/zpi/plagiarism_detector/client/controller/ResultSceneController.java
+++ client/src/main/java/com/zpi/plagiarism_detector/client/controller/ResultSceneController.java
@@ -1,15 +1,13 @@
 package com.zpi.plagiarism_detector.client.controller;
 
-import com.zpi.plagiarism_detector.client.model.ClientModel;
-import com.zpi.plagiarism_detector.client.model.exceptions.CannotConnectToTheServerException;
-import com.zpi.plagiarism_detector.client.model.factories.ClientFactory;
-import com.zpi.plagiarism_detector.client.view.SwitchButton;
-import com.zpi.plagiarism_detector.client.view.ViewUtils;
-import com.zpi.plagiarism_detector.commons.protocol.Message;
-import com.zpi.plagiarism_detector.commons.protocol.ProtocolCode;
 import java.io.IOException;
 import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -19,9 +17,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+import com.zpi.plagiarism_detector.client.model.ClientModel;
+import com.zpi.plagiarism_detector.client.model.factories.ClientFactory;
+import com.zpi.plagiarism_detector.client.view.SwitchButton;
+import com.zpi.plagiarism_detector.commons.protocol.plagiarism.PlagiarismFragment;
+import com.zpi.plagiarism_detector.commons.protocol.plagiarism.PlagiarismResult;
 
 public class ResultSceneController implements Initializable, Controller {
 
@@ -110,5 +113,90 @@ public class ResultSceneController implements Initializable, Controller {
     private void handleBackButtonAction() {
         container.getScene().getWindow().hide();
         MainSceneController.showMainWindow();
+    }
+    
+    /**
+     * Rozdzielenie listy wyników na listy poszczególnych plików (tekst, kody)
+     * @param results
+     * @return
+     */
+    private List<List<PlagiarismResult>> separateDocuments(List<PlagiarismResult> results){
+    	String doc = "";
+    	LinkedList<List<PlagiarismResult>> allResults = new LinkedList<List<PlagiarismResult>>();
+    	LinkedList<PlagiarismResult> docResults;
+    	
+    	while(!results.isEmpty() && doc != null){
+    		doc = null;
+    		docResults = new LinkedList<PlagiarismResult>();
+    		for(PlagiarismResult result: results){
+    			if (result != null){
+    				if (doc == null){
+    					doc = result.getNewDocument();
+    					docResults.add(result);
+    					results.remove(result);
+    				}else{
+    					if(result.getNewDocument().equals(doc)){
+    						docResults.add(result);
+    						results.remove(result);
+    					}
+    				}
+    			}
+    		}
+    		allResults.add(docResults);
+    	}
+    	return allResults;
+   	} 
+    
+    /**
+     * Obliczanie ilości znalezionych zdań/linii podobnych w aktualnie wyświetlanym wyniku
+     *  (ile zdań/linii w artykule podanym jest podobnych do zdań/linii w artykule znalezionym - właśnie wyświetlanym)
+     * @param result
+     * @return
+     */
+    private int getAmountOfSimilarSentences(PlagiarismResult result){
+    	return result.getPlagiarisedFragments().size();
+    }
+    
+    /**
+     * Obliczanie ilości znalezionych zdań/linii podobnych z zdaniami/liniami we wszystkich znalezionych dokumentach.
+     * Wynik dla poszczególnego pliku
+     * @param results
+     * @return
+     */
+    private int getAmountOfSimilarSentencesInAllResults(List<PlagiarismResult> results){
+    	int amount = 0;
+    	for (PlagiarismResult result: results){
+    		amount += getAmountOfSimilarSentences(result);
+    	}
+    	return amount;
+    }
+    
+    /**
+     * Obliczanie procentu długości znalezionych zdań/linii podobnych z aktualnie wyświetlanym 
+     * znalezionym dokumentem w stosunku do długości przesłanego pliku
+     * @param result
+     * @return
+     */
+    private int getPercantageOfSimilarity(PlagiarismResult result){
+    	int fragmentsLength = 0;
+    	for (Entry<PlagiarismFragment, PlagiarismFragment> fragments : result.getPlagiarisedFragments().entrySet()){
+    		fragmentsLength += fragments.getKey().getSize();
+    	}
+    	return fragmentsLength/result.getNewDocument().length();
+    }
+    
+    /**
+     * Obliczanie procentu długości znalezionych zdań/linii podobnych z zdaniami/liniami 
+     * we wszystkich znalezionych dokumentach, w stosunku do długości przesłanego pliku
+     * Wynik dla poszczególnego pliku
+     * @param results
+     * @return
+     */
+    private int getPercantageOfSimilarityInAllResults(List<PlagiarismResult> results){
+    	int fragmentsLength = 0;
+    	for (PlagiarismResult result: results){
+    		fragmentsLength += getPercantageOfSimilarity(result);
+    	}
+    	return fragmentsLength/results.get(0).getNewDocument().length();
     }
 }
