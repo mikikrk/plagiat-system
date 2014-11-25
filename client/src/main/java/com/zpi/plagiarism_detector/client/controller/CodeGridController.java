14fd6475364373a47372524eb17d8174f0e037e9
agat48 <agat.paciorek@gmail.com>
2014-11-26 00:19:43 +0100

    GUI switching codes result

--- client/src/main/java/com/zpi/plagiarism_detector/client/controller/CodeGridController.java
+++ client/src/main/java/com/zpi/plagiarism_detector/client/controller/CodeGridController.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.zpi.plagiarism_detector.client.controller;
 
 import com.zpi.plagiarism_detector.commons.database.DocumentType;
@@ -12,11 +11,16 @@ import java.net.URL;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
+import javafx.util.converter.NumberStringConverter;
 
 /**
  * FXML Controller class
@@ -24,21 +28,33 @@ import javafx.scene.layout.GridPane;
  * @author Agat
  */
 public class CodeGridController implements Initializable {
+
     @FXML
     private GridPane inputArticleGrid;
     @FXML
-    private Label currentArticleNr;
+    private Button prevCodeOut;
     @FXML
-    private Label totalArticleCnt;
+    private Button nextCodeOut;
     @FXML
-    private TextArea newCode;
+    private Label currentCodeNrOut;
     @FXML
-    private Label currentArticleNr1;
-    @FXML
-    private Label totalArticleCnt1;
+    private Label totalCodeCntOut;
     @FXML
     private TextArea foundCode;
+    @FXML
+    private Button prevCodeIn;
+    @FXML
+    private Button nextCodeIn;
+    @FXML
+    private Label currentCodeNrIn;
+    @FXML
+    private Label totalCodeCntIn;
+    @FXML
+    private TextArea newCode;
     private List<List<PlagiarismResult>> allResults;
+    List<PlagiarismResult> currentCodeList;
+    private SimpleIntegerProperty currCodeIndexIn = new SimpleIntegerProperty(1), totalCodeIndexIn = new SimpleIntegerProperty(1);
+    private SimpleIntegerProperty currCodeIndexOut = new SimpleIntegerProperty(1), totalCodeIndexOut = new SimpleIntegerProperty(1);
 
     /**
      * Initializes the controller class.
@@ -46,13 +62,28 @@ public class CodeGridController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         allResults = ResultSceneController.getSeparatedDocuments();
-        List<PlagiarismResult> allData = separateCodes().get(0);
-        if (allData.size() > 0) {
-            newCode.setText(allData.get(0).getNewDocument());
-            foundCode.setText(allData.get(0).getExistingDocument());
-            System.out.println("Mikiiiiiiiiiiiii! <3");
+        allResults = separateCodes();
+        currentCodeList = allResults.get(0);
+        if (currentCodeList.size() > 0) {
+            newCode.setText(currentCodeList.get(0).getNewDocument());
+            foundCode.setText(currentCodeList.get(0).getExistingDocument());
         }
-    }    
+        totalCodeIndexIn.set(allResults.size());
+        totalCodeIndexOut.set(currentCodeList.size());
+        Bindings.bindBidirectional(currentCodeNrIn.textProperty(),
+                currCodeIndexIn,
+                new NumberStringConverter());
+        Bindings.bindBidirectional(totalCodeCntIn.textProperty(),
+                totalCodeIndexIn,
+                new NumberStringConverter());
+        Bindings.bindBidirectional(currentCodeNrOut.textProperty(),
+                currCodeIndexOut,
+                new NumberStringConverter());
+        Bindings.bindBidirectional(totalCodeCntOut.textProperty(),
+                totalCodeIndexOut,
+                new NumberStringConverter());
+    }
+
     private List<List<PlagiarismResult>> separateCodes() {
         LinkedList<List<PlagiarismResult>> filteredArticles = new LinkedList<List<PlagiarismResult>>();
         List<PlagiarismResult> tempList = new LinkedList<PlagiarismResult>();
@@ -60,13 +91,51 @@ public class CodeGridController implements Initializable {
             System.out.println(resultList.toString());
             for (PlagiarismResult singleResult : resultList) {
                 if (singleResult.getType().equals(DocumentType.CODE)) {
-                    if (tempList.size() > 0) {
-                        tempList.add(singleResult);
-                    }
+                    tempList.add(singleResult);
                 }
             }
             filteredArticles.add(tempList);
         }
         return filteredArticles;
+    }
+
+    @FXML
+    private void handlePrevCodeOutAction(ActionEvent event) {
+        if (currCodeIndexOut.get() > 1) {
+            currCodeIndexOut.set(currCodeIndexOut.get() - 1);
+            foundCode.setText(currentCodeList.get(currCodeIndexOut.get() - 1).getExistingDocument());
+        }
+    }
+
+    @FXML
+    private void handleNextCodeOutAction(ActionEvent event) {
+        if (currCodeIndexOut.get() < totalCodeIndexOut.get()) {
+            currCodeIndexOut.set(currCodeIndexOut.get() + 1);
+            foundCode.setText(currentCodeList.get(currCodeIndexOut.get() - 1).getExistingDocument());
+        }
+    }
+
+    @FXML
+    private void handlePrevCodeInAction(ActionEvent event) {
+        if (currCodeIndexIn.get() > 1) {
+            currCodeIndexIn.set(currCodeIndexIn.get() - 1);
+            currentCodeList = allResults.get(currCodeIndexIn.get() - 1);
+            currCodeIndexOut.set(1);
+            totalCodeIndexOut.set(currentCodeList.size());
+            newCode.setText(currentCodeList.get(0).getNewDocument());
+            foundCode.setText(currentCodeList.get(0).getExistingDocument());
+        }
+    }
+
+    @FXML
+    private void handleNextCodeInAction(ActionEvent event) {
+        if (currCodeIndexIn.get() < totalCodeIndexIn.get()) {
+            currCodeIndexIn.set(currCodeIndexIn.get() + 1);
+            currentCodeList = allResults.get(currCodeIndexIn.get() - 1);
+            currCodeIndexOut.set(1);
+            totalCodeIndexOut.set(currentCodeList.size());
+            newCode.setText(currentCodeList.get(0).getNewDocument());
+            foundCode.setText(currentCodeList.get(0).getExistingDocument());
+        }
     }
 }
